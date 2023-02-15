b49e5813be646e87eb91272970ef8714b1bbf93b
HQX01 <hqx666@bupt.edu.cn>
2023-02-15 10:56:07 +0800

    chore: test github action

--- src/component/accountPage/index.tsx
+++ src/component/accountPage/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function AccountPage() {
     return (
         <div className="account">
-            accountPage
+            test github action
         </div>
     );
 }
